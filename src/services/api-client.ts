edaf025727eeb9c9bad0435d6b16da8ea7fd64b4
david_kr <david_kr@optimove.com>
2024-08-07 22:09:46 +0300

    add env

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params:{
-        key: '15f7a5866a2e4f5797fa583cf0799c50'
+        key: import.meta.env.VITE_RAWG_API_KEY
     }
 })
